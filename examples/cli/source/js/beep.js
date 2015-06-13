d78acdaf59d10f860a3d0b628d20b3c41ef347e8
Eric Bednarz <github@bednarz.nl>
2015-06-13 12:22:14 +0200

    Clean up dummy modules a bit

--- examples/cli/source/js/beep.js
+++ examples/cli/source/js/beep.js
@@ -1,13 +1,6 @@
 import domready from 'domready';
 import foo from '_app/foo';
 
-/**
- * JSDoc
- */
 domready(function () {
-    // line comment
-    foo(); // out of place comment
+    foo(12);
 });
-
-// trailing comment
-
